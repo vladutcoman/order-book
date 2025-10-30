43769a3b85e10f4d7acd928acc7c11ff15a364ca
vladutcoman <coman.vladut4@gmail.com>
2025-10-30 19:13:46 +0200

    updates

--- src/store/orderBook/useOrderBookStore.ts
+++ src/store/orderBook/useOrderBookStore.ts
@@ -1,14 +1,46 @@
-import type { OrderBookTab } from "@/types/orderBookTypes";
+import type {
+  OrderBookDecimal,
+  OrderBookDepthVisualization,
+  OrderBookTab,
+} from "@/types/orderBookTypes";
 import { create } from "zustand";
 
 interface OrderBookStore {
   tab: OrderBookTab;
+  decimal: OrderBookDecimal;
+  depthVisualization: OrderBookDepthVisualization;
+  displayAvgSum: boolean;
+  showBuySellRatio: boolean;
+  animationsEnabled: boolean;
+  rounding: boolean;
+
   setTab: (tab: OrderBookTab) => void;
+  setDecimal: (decimal: OrderBookDecimal) => void;
+  setDepthVisualization: (
+    depthVisualization: OrderBookDepthVisualization,
+  ) => void;
+  setDisplayAvgSum: (displayAvgSum: boolean) => void;
+  setShowBuySellRatio: (showBuySellRatio: boolean) => void;
+  setAnimationsEnabled: (animationsEnabled: boolean) => void;
+  setRounding: (rounding: boolean) => void;
 }
 
 const useOrderBookStore = create<OrderBookStore>((set) => ({
   tab: "both",
+  decimal: 0.001,
+  depthVisualization: "amount",
+  displayAvgSum: false,
+  showBuySellRatio: false,
+  animationsEnabled: true,
+  rounding: false,
+
   setTab: (tab) => set({ tab }),
+  setDecimal: (decimal) => set({ decimal }),
+  setDepthVisualization: (depthVisualization) => set({ depthVisualization }),
+  setDisplayAvgSum: (displayAvgSum) => set({ displayAvgSum }),
+  setShowBuySellRatio: (showBuySellRatio) => set({ showBuySellRatio }),
+  setAnimationsEnabled: (animationsEnabled) => set({ animationsEnabled }),
+  setRounding: (rounding) => set({ rounding }),
 }));
 
 export default useOrderBookStore;
