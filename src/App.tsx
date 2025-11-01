b73816d4fc8dff04ca5a4512df210e9f13f2f5ad
vladutcoman <coman.vladut4@gmail.com>
2025-11-01 19:24:18 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import OrderBook from "@/features/OrderBook/OrderBook";
 
 function App() {
   return (
-    <main className="container mx-auto max-w-6xl p-4 md:p-6 lg:p-8">
-      <div className="flex flex-col gap-10">
-        <MarketSelector />
-        <OrderBook />
+    <main className="container mx-auto p-4 md:p-6 lg:p-8">
+      <div className="flex flex-col md:flex-row gap-20">
+        <div className="flex flex-1 flex-col w-full gap-2 h-full items-center">
+          <p className="text-sm text-muted-foreground">
+            Welcome to Vladut's Order Book
+          </p>
+          <MarketSelector />
+        </div>
+        <div className="flex flex-2 flex-col">
+          <OrderBook />
+        </div>
       </div>
     </main>
   );
